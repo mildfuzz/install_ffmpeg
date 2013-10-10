fb0d3448805721400a222fb78d3dbd8cdc49768d
Josh Frye <joshfng@gmail.com>
2013-10-10 11:03:51 -0400

    Update install_ffmpeg.sh

--- install_ffmpeg.sh
+++ install_ffmpeg.sh
@@ -4,69 +4,45 @@
 sudo apt-get remove ffmpeg x264 libav-tools libvpx-dev libx264-dev yasm
 
 sudo apt-get update
-sudo apt-get -y install autoconf automake build-essential make checkinstall git libass-dev libfaac-dev \
-  libgpac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev librtmp-dev libspeex-dev \
-  libtheora-dev libtool libvorbis-dev pkg-config texi2html zlib1g-dev libass-dev libfaac-dev \
-  libspeex-dev
+sudo apt-get -y install autoconf automake build-essential git libass-dev libgpac-dev \
+  libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libx11-dev \
+  libxext-dev libxfixes-dev pkg-config texi2html zlib1g-dev yasm libmp3lame-dev 
+  libopus-dev libvpx-dev
 
-cd
-wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
-tar xzvf yasm-1.2.0.tar.gz
-cd yasm-1.2.0
-./configure
-make
-sudo checkinstall --pkgname=yasm --pkgversion="1.2.0" --backup=no \
-  --deldoc=yes --fstrans=no --default
+mkdir ~/ffmpeg_sources
 
-cd
+cd ~/ffmpeg_sources
 git clone --depth 1 git://git.videolan.org/x264.git
 cd x264
-./configure --enable-static
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin" --enable-static
 make
-sudo checkinstall --pkgname=x264 --pkgversion="3:$(./version.sh | \
-  awk -F'[" ]' '/POINT/{print $4"+git"$5}')" --backup=no --deldoc=yes \
-  --fstrans=no --default
+make install
+make distclean
 
-cd
+cd ~/ffmpeg_sources
 git clone --depth 1 git://github.com/mstorsjo/fdk-aac.git
 cd fdk-aac
 autoreconf -fiv
-./configure --disable-shared
+./configure --prefix="$HOME/ffmpeg_build" --disable-shared
 make
-sudo checkinstall --pkgname=fdk-aac --pkgversion="$(date +%Y%m%d%H%M)-git" --backup=no \
-  --deldoc=yes --fstrans=no --default
+make install
+make distclean
 
-cd
-git clone --depth 1 http://git.chromium.org/webm/libvpx.git
-cd libvpx
-./configure --disable-examples --disable-unit-tests
-make
-sudo checkinstall --pkgname=libvpx --pkgversion="1:$(date +%Y%m%d%H%M)-git" --backup=no \
-  --deldoc=yes --fstrans=no --default
-
-cd
+cd ~/ffmpeg_sources
 git clone --depth 1 git://source.ffmpeg.org/ffmpeg
 cd ffmpeg
-./configure --enable-gpl --enable-libass --enable-libfaac --enable-libfdk-aac --enable-libmp3lame \
-  --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libspeex --enable-librtmp --enable-libtheora \
-  --enable-libvorbis --enable-libvpx --enable-libx264 --enable-nonfree --enable-version3
+PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
+export PKG_CONFIG_PATH
+./configure --prefix="$HOME/ffmpeg_build" \
+  --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
+  --bindir="$HOME/bin" --extra-libs="-ldl" --enable-gpl --enable-libass --enable-libfdk-aac \
+  --enable-libmp3lame --enable-libopus --enable-libtheora --enable-libvorbis --enable-libvpx \
+  --enable-libx264 --enable-nonfree --enable-x11grab
 make
-sudo checkinstall --pkgname=ffmpeg --pkgversion="7:$(date +%Y%m%d%H%M)-git" --backup=no \
-  --deldoc=yes --fstrans=no --default
+make install
+make distclean
 hash -r
 
-cd ~/ffmpeg
-make tools/qt-faststart
-sudo checkinstall --pkgname=qt-faststart --pkgversion="$(date +%Y%m%d%H%M)-git" --backup=no \
-  --deldoc=yes --fstrans=no --default install -Dm755 tools/qt-faststart \
-  /usr/local/bin/qt-faststart
+$HOME/bin/ffmpeg 2>&1 | head -n1
 
-cd ~/x264
-make distclean
-./configure --enable-static
-make
-sudo checkinstall --pkgname=x264 --pkgversion="3:$(./version.sh | \
-  awk -F'[" ]' '/POINT/{print $4"+git"$5}')" --backup=no --deldoc=yes \
-  --fstrans=no --default
-
-echo "DONE\n"
+echo "\nDone\n"
